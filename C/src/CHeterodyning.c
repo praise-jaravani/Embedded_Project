df314f93e6efb14f152e59671318e11d1ca9ece9
Praise Jaravani <jaravanipraise@gmail.com>
2024-10-21 21:37:28 +0200

    Testing different data types

--- C/src/CHeterodyning.c
+++ C/src/CHeterodyning.c
@@ -1,4 +1,8 @@
 #include "CHeterodyning.h"
+#include <stdio.h>
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
 
 extern DATA_TYPE data[SAMPLE_COUNT];
 extern DATA_TYPE carrier[SAMPLE_COUNT];
@@ -7,7 +11,7 @@ DATA_TYPE result[SAMPLE_COUNT];
 
 int main(int argc, char**argv) {
     printf("Running Unthreaded Test\n");
-    printf("Data Type: %s\n", #DATA_TYPE);
+    printf("Data Type: %s\n", TOSTRING(DATA_TYPE));
     printf("Size of data type: %zu bits\n", sizeof(DATA_TYPE) * 8);
     printf("Total amount of samples: %d\n", SAMPLE_COUNT);
 
@@ -16,7 +20,15 @@ int main(int argc, char**argv) {
         result[i] = data[i] * carrier[i];
     }
     double t = toc();
+    
     printf("Time: %lf ms\n", t/1e-3);
+    
+    // Optional: Print a few results to verify correctness
+    printf("First few results:\n");
+    for (int i = 0; i < 5 && i < SAMPLE_COUNT; i++) {
+        printf("result[%d] = %f\n", i, (double)result[i]);
+    }
+    
     printf("End Unthreaded Test\n");
     return 0;
 }
