70540222d808111cb2b5c5c3cf5e8e4af5ea2bd9
Praise Jaravani <jaravanipraise@gmail.com>
2024-10-21 21:34:03 +0200

    Testing different data types

--- C/src/CHeterodyning.c
+++ C/src/CHeterodyning.c
@@ -1,47 +1,18 @@
-/* #include "CHeterodyning.h"
-
-extern float data [SAMPLE_COUNT];
-extern float carrier[SAMPLE_COUNT];
-
-float result [SAMPLE_COUNT];
-
-int main(int argc, char**argv){
-    printf("Running Unthreaded Test\n");
-    printf("Precision sizeof %ld\n", sizeof(float));
-    printf("Size of float: %zu bits\n", sizeof(float) * 8);
-    
-
-    printf("Total amount of samples: %ld\n", sizeof(data) / sizeof(data[0]));
-    
-
-    tic(); // start the timer
-    for (int i = 0;i<SAMPLE_COUNT;i++ ){
-        result[i] = data[i] * carrier[i];
-    }
-    double t = toc();
-    printf("Time: %lf ms\n",t/1e-3);
-    printf("End Unthreaded Test\n");
-    return 0;
-} */
-
-// For Altered Data Types
-
 #include "CHeterodyning.h"
 
-extern data_t data [SAMPLE_COUNT];
-extern data_t carrier[SAMPLE_COUNT];
+extern DATA_TYPE data[SAMPLE_COUNT];
+extern DATA_TYPE carrier[SAMPLE_COUNT];
 
-data_t result [SAMPLE_COUNT];
+DATA_TYPE result[SAMPLE_COUNT];
 
 int main(int argc, char**argv) {
     printf("Running Unthreaded Test\n");
-    printf("Precision sizeof %zu bytes\n", sizeof(data_t));
-    printf("Size of data_t: %zu bits\n", sizeof(data_t) * 8);
-    
-    printf("Total amount of samples: %zu\n", sizeof(data) / sizeof(data[0]));
-    
+    printf("Data Type: %s\n", #DATA_TYPE);
+    printf("Size of data type: %zu bits\n", sizeof(DATA_TYPE) * 8);
+    printf("Total amount of samples: %d\n", SAMPLE_COUNT);
+
     tic(); // start the timer
-    for (int i = 0; i < SAMPLE_COUNT; i++ ) {
+    for (int i = 0; i < SAMPLE_COUNT; i++) {
         result[i] = data[i] * carrier[i];
     }
     double t = toc();
--- C/src/CHeterodyning.h
+++ C/src/CHeterodyning.h
@@ -1,22 +1,3 @@
-/* #ifndef Prac2_threadded_h
-#define Prac2_threadded_h
-//------------------------------------------------------------------------------
-
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-//------------------------------------------------------------------------------
-
-#include "Timer.h"
-#include "globals.h"
-//------------------------------------------------------------------------------
-
-
-#endif
- */
-
-// For altered data types
- 
 #ifndef Prac2_threadded_h
 #define Prac2_threadded_h
 
@@ -27,20 +8,8 @@
 #include "Timer.h"
 #include "globals.h"
 
-#ifdef USE_FLOAT
-    typedef float data_t;
-#elif defined(USE_DOUBLE)
-    typedef double data_t;
-#elif defined(USE_LONG_DOUBLE)
-    typedef long double data_t;
-#elif defined(USE_FP16)
-    #include <arm_fp16.h>
-    typedef __fp16 data_t;
-#elif defined(USE_FLOAT128)
-    #include <quadmath.h>
-    typedef __float128 data_t;
-#else
-    typedef float data_t;
+#ifndef DATA_TYPE
+#define DATA_TYPE float
 #endif
 
 #endif
