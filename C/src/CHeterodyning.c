5fd153139be6ac2b50923d1f29d35a09d0d3aaf6
Praise Jaravani <jaravanipraise@gmail.com>
2024-10-22 10:12:37 +0200

    Combinations of optimisations

--- C/src/CHeterodyning.c
+++ C/src/CHeterodyning.c
@@ -1,34 +1,24 @@
 #include "CHeterodyning.h"
-#include <stdio.h>
 
-#define STRINGIFY(x) #x
-#define TOSTRING(x) STRINGIFY(x)
+extern float data [SAMPLE_COUNT];
+extern float carrier[SAMPLE_COUNT];
 
-extern DATA_TYPE data[SAMPLE_COUNT];
-extern DATA_TYPE carrier[SAMPLE_COUNT];
+float result [SAMPLE_COUNT];
 
-DATA_TYPE result[SAMPLE_COUNT];
-
-int main(int argc, char**argv) {
+int main(int argc, char**argv){
     printf("Running Unthreaded Test\n");
-    printf("Data Type: %s\n", TOSTRING(DATA_TYPE));
-    printf("Size of data type: %zu bits\n", sizeof(DATA_TYPE) * 8);
-    printf("Total amount of samples: %d\n", SAMPLE_COUNT);
+    printf("Precision sizeof %ld\n", sizeof(float));
+    
+
+    printf("Total amount of samples: %ld\n", sizeof(data) / sizeof(data[0]));
+    
 
     tic(); // start the timer
-    for (int i = 0; i < SAMPLE_COUNT; i++) {
+    for (int i = 0;i<SAMPLE_COUNT;i++ ){
         result[i] = data[i] * carrier[i];
     }
     double t = toc();
-    
-    printf("Time: %lf ms\n", t/1e-3);
-    
-    /* // Optional: Print a few results to verify correctness
-    printf("First few results:\n");
-    for (int i = 0; i < 5 && i < SAMPLE_COUNT; i++) {
-        printf("result[%d] = %f\n", i, (double)result[i]);
-    } */
-    
+    printf("Time: %lf ms\n",t/1e-3);
     printf("End Unthreaded Test\n");
     return 0;
 }
